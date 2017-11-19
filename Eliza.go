6aa4039da6be46726abade61d31e51b7dfa0038a
Kevin@test.com <kevin@test.com>
2017-11-19 14:01:02 +0000

    Code refactered to reflect the captured part of the string

--- Eliza.go
+++ Eliza.go
@@ -10,6 +10,8 @@
 *https://stackoverflow.com/questions/23805443/remove-the-form-input-fields-data-after-click-on-submit
 *https://stackoverflow.com/questions/12430907/create-div-using-form-data-with-ajax-jquery
 *https://stackoverflow.com/questions/42018775/pattern-matching-and-regular-expression-in-perl
+*https://gist.github.com/ianmcloughlin/c4c2b8dc586d06943f54b75d9e2250fe
+*https://github.com/data-representation/eliza/blob/master/data/responses.txt
  */
 
 package main
@@ -26,17 +28,18 @@ import (
 func Reflections(capturedString string) string {
 	//adapted from https://stackoverflow.com/questions/10196462/regex-word-boundary-excluding-the-hyphen
 	//To prevent "you're"  or any word with a "'" from getting split into three tokens
+	//Adapted from https://gist.github.com/ianmcloughlin/c4c2b8dc586d06943f54b75d9e2250fe
 	boundaries := regexp.MustCompile(`(\b[^\w']|$)`)
 	tokens := boundaries.Split(capturedString, -1)
 
 	// List the reflections.
 	reflections := [][]string{
 		{`\bare\b`, `am`},
-		{`\bam/b`, `are`},
+		{`\bam\b`, `are`},
 		{`\bwere\b`, `was`},
 		{`\bwas\b`, `were`},
 		{`\byou\b`, `I`},
-		{`\bI\b`, `you`},
+		{`\b(?i)I\b`, `you`},
 		{`\bme\b`, `you`},
 		{`\byour\b`, ` my`},
 		{`\bmy\b`, `your`},
@@ -68,54 +71,59 @@ func Reflections(capturedString string) string {
 
 //Function ElizaResponse to take in and return a string
 func ElizaResponse(str string) string {
-	//Adapted from https://gist.github.com/ianmcloughlin/c4c2b8dc586d06943f54b75d9e2250fe
-
-	/*Regex MatchString function with isolation of the word "father"
-	*with a boundry ignore case regex command.
+	/*The below regular expression was adapted from https://github.com/data-representation/eliza/blob/master/data/responses.txt
+	 *Compile the regular expression that says to match while ingnoring case and multi line.(?im)
+	 *At the start of a line 0 or more whitespace followed by the characters I can with one or zero "'".\s*I can'?t
+	 *followed by t.
+	 *capture anything that is not (. !) zero or more times.([^\.!]*)
+	 *followed by (. !) zero or more times [\.!]* followed by whitespace zero or more times.\s*
+	 *Finally end of the line.$
 	 */
-	if matched, _ := regexp.MatchString(`(?i)\bfather\b`, str);
-	//Condition to replace the original string if it has the word "father"
-	matched {
-		return "Why don’t you tell me more about your father?"
-	}
-	r1 := regexp.MustCompile(`(?i)^\bI'?\s*a?m\b`)
+	r1 := regexp.MustCompile(`(?im)^\s*I can'?t ([^\.!]*)[\.!]*\s*$`)
 
-	//Match the words "I am" and capture for replacement
+	//Match the words "I can't" and capture for replacement
 	matched := r1.MatchString(str)
 
-	//condition if "I am" is matched
+	//condition if "I can't" is matched
 	if matched {
-		replacementString := "How do you know that you are"
-		//Only keep the captured part of the string
-		//Pass in everything after the captured part of the statement to the function Reflections
-		response := Reflections(r1.ReplaceAllString(str, "$1"))
-		//Concat the new opening line at the end of the function
-		return replacementString + response
+		/*Change my to your, i to you etc...
+		*pass the captured string into ReplaceAllString function
+		*against the original string.
+		*This then gets passed to the reflections function to change nouns.
+		 */
+		reflectString := Reflections(r1.ReplaceAllString(str, "$1"))
+		//Test output of string commimg out of Reflections
+		//fmt.Println(reflectString)
+
+		//replace and create the new string with the answer and changed nouns.
+		response := r1.ReplaceAllString(str, "Perhaps you could "+reflectString+" if you tried.")
+		//return final string to the front end.
+		return response
 	}
-	//Capture and replace How are you
-	r1 = regexp.MustCompile(`\bI have\b`)
+	//Capture and replace I have
+	//The below regular expression was adapted from https://github.com/data-representation/eliza/blob/master/data/responses.txt
+	r1 = regexp.MustCompile(`(?im)^\s*I have ([^\.!]*)[\.!]*\s*$`)
 	matched = r1.MatchString(str)
 	if matched {
-		replacementString := "How long have you had "
+		reflectString := Reflections(r1.ReplaceAllString(str, "$1"))
+
 		//Only keep the captured part of the string
 		//Pass in everything after the captured part of the statement to the function Reflections
-		response := Reflections(r1.ReplaceAllString(str, "$1"))
+		response := r1.ReplaceAllString(str, "How long have you had "+reflectString+"?")
 		//Concat the new opening line at the end of the function
-		return replacementString + response
+		return response
 	}
 	//Capture and replace How are you
-	r1 = regexp.MustCompile(`\bMy name is\b`)
+	r1 = regexp.MustCompile(`(?im)^\s*My name is ([^\.!]*)[\.!]*\s*$`)
 	matched = r1.MatchString(str)
 	if matched {
-		replacementString := "Hello "
+		reflectString := Reflections(r1.ReplaceAllString(str, "$1"))
+
 		//Only keep the captured part of the string
 		//Pass in everything after the captured part of the statement to the function Reflections
-		response := Reflections(r1.ReplaceAllString(str, "$1"))
-		if response == "" {
-			return "I didn't get your name can you please tell me again"
-		}
+		response := r1.ReplaceAllString(str, "Hello "+reflectString+" how have you been?")
 		//Concat the new opening line at the end of the function
-		return replacementString + response
+		return response
 	}
 	//Get random number from the length of the array of random struct
 	//an array of strings for the random response
@@ -127,6 +135,8 @@ func ElizaResponse(str string) string {
 		"Can you understand me?",
 		"How can I help?",
 		"Are you still there?",
+		"What have you been up to recently?",
+		"How are you finding the conversation we are having",
 	}
 	//Return a random index of the array
 	return randomResponse[rand.Intn(len(randomResponse))]
